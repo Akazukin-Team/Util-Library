aaf0501a92922401155b66d0eacfa2f25c904f50
Currypan1229 <67533379+Currypan1229@users.noreply.github.com>
2025-06-21 17:21:39 +0900

    Add Triple

---
+++ util/src/main/java/org/akazukin/util/object/Triple.java
@@ -0,0 +1,32 @@
+package org.akazukin.util.object;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+import lombok.experimental.FieldDefaults;
+import org.akazukin.annotation.marker.NonThreadSafe;
+
+/**
+ * Represents a generic pair of three elements.
+ * <p>
+ * This class is a simple container object that holds three related objects.
+ * It is designed to be immutable and provides
+ * convenience methods for object comparison and string representation.
+ *
+ * @param <F> the type of the first element.
+ * @param <S> the type of the second element.
+ * @param <T> the type of the third element.
+ */
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Getter
+@ToString
+@EqualsAndHashCode
+@NonThreadSafe
+public class Triple<F, S, T> {
+    F first;
+    S second;
+    T third;
+}
